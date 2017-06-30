2b97837b1e1990b1a4b31c1abec5bf823d952703
Mason Galland <masongalland@me.com>
2017-06-30 10:31:28 -0600

    set up new repo on github

---
+++ assessment.js
@@ -0,0 +1,95 @@
+// #1 Create a var called 'up' that says 'down'
+
+
+
+// #2 Use the var iHeart to create a new var called iHeartJavascript that says 'I heart javascript'
+var iHeart = 'I heart';
+
+// #3  Make an object called developer
+// developer has a title of Junior (String)
+// developer has a salary of 55000 (Number)
+// developer has a laptop, that has type Macbook, and year 2016 (object)
+// developer is not billGates (boolean)
+// developer likes isolation, caffeine, and pizza (array of strings)
+
+
+// #4 Change my dog's name to Fido using dot notation
+var myDog = {
+	type: 'beagle',
+	name: 'Spot'
+};
+
+// Change my don't type to hairless using square bracket notation
+var myOtherDog = {
+	type: 'poodle',
+	name: 'Fluffy'
+};
+
+// #5 Create an object that tracks a count of kids in each grade in a school.  Call it 'school'
+// The key should be the grade (string) and the value should be how many kids there are.
+// Our school has 50 kids in first, 30 kids in second and 80 kids in third
+
+
+
+// #6 Loop through this object and change every property that begins with the word 'secret' to have a value of 'redacted'
+var briefing = {
+	secretLocation: 'antarctica',
+  secretWeapon: 'hands',
+	nonsecretCodename: 'purple hippopotamus',
+	secretInfo: "it's gon' be cold",
+  date: 'future',
+};
+
+
+// #7 Create an array of strings that are the 7 days of the week. Call your array daysOfWeek
+
+// #8 Using this array do the following
+var jedi = ['Yoda', 'Mace Windu', 'Obi-Wan'];
+// add 'Luke' to the end
+
+// remove 'Yoda' and store him in a var called firstJedi
+
+// add 'Mario' to the start of the array
+
+// remove 'Obi-Wan' from the array and store him in a var called secondJedi
+
+// leave 'Mace Windu' in the array but put a copy of him on a var called thirdJedi
+
+
+
+
+// #9 Write a function called cutInLine, that takes in an array and an item, and adds the item
+// after the first item in the array and return the array
+// EX: cutInLine([1,2,3,4], 8) => [1,8,2,3,4];
+
+
+// #10 Write a function called removeThings, that takes in an array of things (strings or numbers), and a 'thing' (a string or a number).
+// Removes all instances of that 'thing' from the array. And return the modified array.
+// The order of the array should not be changed
+// removeThings(['a', 1, 4, 'a'], 'a') => [1,4]
+
+
+// #11 Write a function tripleTheFun that takes 1 parameter: a number or a string. It triples numbers, and
+// repeats strings. Example: 4->12, 2.5->7.5, 'Awesome'->'AwesomeAwesomeAwesome'
+
+
+// #12 Write function getPropertyValue that takes in an object, and the name of a property on the object as a string
+// return the value from the object that corresponds to the property
+
+// #13 Write a function called clockIn that takes in an employee and department name
+// and returns an object with a name, department, and timestamp which is
+// the current time as a Date object
+
+// #14 Create a function called spiderTest that takes in an object that is an actor, it looks to see if the actor's name is Andrew Garfield and changes the actor object to have a property called spiderRating with a value of 10.  If their name is Tobey Maguire set spiderRating to 0.  otherwise set spiderRating to 'not a spiderman'.
+
+// #15 Create a function called whatIsItOutside that takes in a temperature (number), a humidity(number), and a cloudiness(number), in that order. Using the following to return the correct values
+/*
+    temperature over 85 and humidity over 45 - return "I'm all sweat"
+    temperature under 40 and cloudiness over 60 - return "I have icicles"
+    temperature over 85 and humidity under 45 and cloudiness under 20 - return "I'm literally in the desert"
+    temperature over 90 or humidity over 55 or cloudiness over 65 - return "Hmm, probably not"
+    Otherwise - return "I love outside"
+*/
+
+// #16 Create a function called callerBack that takes in a function (holla) and a string parameter(back) and invokes it(holla) with the string(back) + ' back'.
+// IE - If I call you with 'Give it' you should invoke holla with 'Give it back'
---
+++ test/spec/javascriptBasicAssessment.js
@@ -0,0 +1,292 @@
+describe('javascriptBasicAssesment', function () {
+    describe('up', function () {
+		it('should exist', function () {
+			expect(up).toEqual(jasmine.any(String));
+		});
+		it('should equal \'down\'', function () {
+			expect(up).toEqual('down');
+		});
+	});
+
+	describe('iHeartJavascript', function () {
+		it('should exist', function () {
+			expect(iHeart).toEqual(jasmine.any(String));
+		});
+		it('should equal \'I heart javascript\'', function () {
+			expect(iHeartJavascript).toEqual('I heart javascript');
+		});
+	});
+
+	describe('developer', function () {
+		it('should exist', function () {
+			expect(developer).toBeDefined();
+		});
+		it('should be as described', function () {
+			expect(developer.title.toLowerCase()).toEqual("junior");
+      expect(developer.salary).toEqual(jasmine.any(Number));
+			expect(developer.salary).toEqual(55000);
+			expect(developer.laptop.type.toLowerCase()).toEqual("macbook");
+			// expect(developer.laptop.year).toEqual("2016");
+      expect(developer.laptop.year == '2016').toBe(true);
+			expect(developer.billGates).toEqual(false);
+			expect(developer.likes).toContain('isolation');
+			expect(developer.likes).toContain('caffeine');
+			expect(developer.likes).toContain('pizza');
+		});
+	});
+
+	describe('dogs', function () {
+		it('should have a name of Fido', function () {
+			expect(myDog.name.toLowerCase()).toEqual('fido');
+		});
+
+		it('should be hairless', function () {
+			expect(myOtherDog.type.toLowerCase()).toEqual('hairless');
+		});
+	});
+
+
+	describe('school', function () {
+		it('should exist', function () {
+			expect(school).toEqual(jasmine.any(Object));
+		});
+		it('should have \'first\' grade property', function () {
+			expect(school.hasOwnProperty('first')).toEqual(true);
+		});
+		it('should have \'second\' grade property', function () {
+			expect(school.hasOwnProperty('second')).toEqual(true);
+		});
+
+		it('should have \'third\' grade property', function () {
+			expect(school.hasOwnProperty('third')).toEqual(true);
+		});
+
+		it('should count the children in each grade correctly', function () {
+			expect(school.first).toEqual(50);
+			expect(school.second).toEqual(30);
+			expect(school.third).toEqual(80);
+		});
+	});
+
+	describe('briefing', function () {
+		it('should have 5 properties', function () {
+			var count = 0;
+			for (var prop in briefing) {
+				count++;
+			}
+			expect(count).toEqual(5);
+		});
+		it('should change properties that start with secret', function () {
+			expect(briefing.secretLocation).toEqual('redacted');
+      expect(briefing.secretWeapon).toEqual('redacted');
+      expect(briefing.secretInfo).toEqual('redacted');
+      expect(briefing.nonsecretCodename).toEqual('purple hippopotamus');
+		});
+	});
+
+	describe('daysOfWeek', function () {
+		var check = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'].sort();
+
+		it('should exist', function () {
+			expect(daysOfWeek).toEqual(jasmine.any(Array));
+		});
+
+		it('should have all 7 days of the week', function () {
+      daysOfWeek = daysOfWeek.map(function(value) {
+        return value.toLowerCase();
+      }).sort();
+			expect(daysOfWeek).toEqual(check);
+		});
+	});
+
+	describe('jedi', function () {
+		it('should add \'Luke\' to the end', function () {
+			expect(jedi[jedi.length - 1].toLowerCase()).toEqual('luke');
+		});
+		it('should remove \'Yoda\' and store him in a variable', function () {
+      expect(jedi.includes('Yoda')).toBe(false);
+			expect(firstJedi.toLowerCase()).toEqual('yoda');
+		});
+		it('should add \'Mario\' to the start of the array', function () {
+			expect(jedi[0].toLowerCase()).toEqual('mario');
+		});
+    it('should remove \'Obi-Wan\' and store him in a variable', function () {
+      expect(jedi.includes('Obi-Wan')).toBe(false);
+			expect(secondJedi.toLowerCase()).toEqual('obi-wan');
+		});
+		it('should copy Mace Windu from the array and store him in a variable', function () {
+			expect(thirdJedi.toLowerCase()).toEqual('mace windu');
+			expect(jedi.indexOf('Mace Windu') > 0).toEqual(true);
+		});
+	});
+
+
+
+	describe('cutInLine', function () {
+		it('should exist', function () {
+			expect(cutInLine).toBeDefined();
+		});
+		it('should help an item cut in line', function () {
+			expect(cutInLine([1,2,3,4], 8)).toEqual([1,8,2,3,4]);
+      expect(cutInLine(['first', 'second', 'third'], 'fourth')).toEqual(['first', 'fourth', 'second', 'third']);
+      expect(cutInLine(['a'], 8)).toEqual(['a', 8]);
+      expect(cutInLine([], 8)).toEqual([8]);
+		});
+	});
+
+	describe('removeThings', function () {
+		it('should exist', function () {
+			expect(removeThings).toBeDefined();
+		});
+		it('should remove item', function () {
+			expect(removeThings(["A", "B", "C", "D"], "C")).toEqual(["A", "B", "D"]);
+			expect(removeThings(["A", "B", "C", "D"], "A")).toEqual(["B", "C", "D"]);
+			expect(removeThings(["A", "B", "C", "D"], "E")).toEqual(["A", "B", "C", "D"]);
+      expect(removeThings([1,2,3], 2)).toEqual([1,3]);
+		});
+		it('should remove all instances of the item', function () {
+			expect(removeThings(["A", "C", "A", "D"], "A")).toEqual(["C", "D"]);
+			expect(removeThings(["A", "A", "C", "D"], "A")).toEqual(["C", "D"]);
+			expect(removeThings(["A", "A", "A", "A"], "A")).toEqual([]);
+      expect(removeThings([1,2,3,1], 1)).toEqual([2,3]);
+		});
+	});
+
+
+
+	describe('tripleTheFun', function () {
+		it('should exist', function () {
+			expect(tripleTheFun).toBeDefined();
+		});
+		it('should triple numbers', function () {
+			expect(tripleTheFun(2)).toEqual(6);
+			expect(tripleTheFun(4)).toEqual(12);
+			expect(tripleTheFun(3.5)).toEqual(10.5);
+		});
+		it('should repeat strings', function () {
+			expect(tripleTheFun("A")).toEqual("AAA");
+			expect(tripleTheFun("Double")).toEqual("DoubleDoubleDouble");
+			expect(tripleTheFun("SuchFun")).toEqual("SuchFunSuchFunSuchFun");
+		});
+		it('should triple numbers pretending to be strings', function () {
+			expect(tripleTheFun("1")).toEqual(3);
+			expect(tripleTheFun("2.5")).toEqual(7.5);
+			expect(tripleTheFun("4")).toEqual(12);
+		});
+	});
+
+	describe('getPropertyValue', function () {
+		it('should exist', function () {
+			expect(getPropertyValue).toBeDefined();
+		});
+		it('should return a value of the obj\'s passed in property.', function () {
+			expect(getPropertyValue({ a: 1, b: 2, c: 3 }, 'a')).toEqual(1);
+			expect(getPropertyValue({ name: "Brack", bob: "Finicky", carlos: "What?" }, 'bob')).toEqual("Finicky");
+			expect(getPropertyValue({ name: "Brack", bob: "Finicky", carlos: "What?" }, 'carlos')).toEqual("What?");
+		});
+	});
+
+	describe('clockIn', function () {
+		it('should exist', function () {
+			expect(clockIn).toBeDefined();
+		});
+		it('should make chat message object', function () {
+			expect(clockIn("Elon Musk", "CEO").name).toEqual("Elon Musk");
+			expect(clockIn("Elon Musk", "CEO").department).toEqual("CEO");
+			expect(clockIn("Elon Musk", "CEO").timestamp).toEqual(new Date());
+		});
+	});
+
+
+	describe('spiderTest', function () {
+		it('should exist', function () {
+			expect(spiderTest).toBeDefined();
+		});
+
+		it('accurately rates Andrew Garfield\'s spiderman', function () {
+			var actor = { name: 'Andrew Garfield' };
+			spiderTest(actor);
+
+			expect(actor.spiderRating).toEqual(10);
+		});
+
+		it('undervalues Tobey Maguire', function () {
+			var actor = { name: 'Tobey Maguire' };
+			spiderTest(actor);
+
+			expect(actor.spiderRating).toEqual(0);
+		});
+
+		it('recognizes Jeremy is not spiderman', function () {
+			var actor = { name: 'Jeremy' };
+			spiderTest(actor);
+
+			expect(actor.spiderRating.toLowerCase()).toEqual('not a spiderman');
+		});
+	});
+
+	describe('whatIsItOutside', function () {
+		it('should exist', function () {
+			expect(whatIsItOutside).toBeDefined();
+		});
+
+		it('should warn me I will be sweaty', function () {
+			var res = whatIsItOutside(86, 46);
+
+			expect(res).toEqual("I'm all sweat");
+		});
+
+		it('should say something silly about icicles', function () {
+			var res = whatIsItOutside(10, 0, 90);
+
+			expect(res).toEqual("I have icicles");
+		});
+
+		it('should inform me I am in the desert', function () {
+			var res = whatIsItOutside(86, 44, 19);
+
+			expect(res).toEqual("I'm literally in the desert");
+		});
+
+		it('should make me hesitate about life choices - hot', function () {
+      expect(whatIsItOutside(91, 20, 45)).toEqual("Hmm, probably not");
+		});
+
+		it('should make me hesitate about life choices - humid', function () {
+      expect(whatIsItOutside(80, 56, 45)).toEqual("Hmm, probably not");
+		});
+
+		it('should make me hesitate about life choices - cloudy', function () {
+      expect(whatIsItOutside(80, 20, 66)).toEqual("Hmm, probably not");
+		});
+
+		it('should encourage me to go whatIsItOutside', function () {
+			expect(whatIsItOutside(79, 20, 45)).toEqual("I love outside");
+		});
+	});
+
+	describe('callerBack', function () {
+		it('should exist', function () {
+			expect(callerBack).toBeDefined();
+		});
+		it('should call me back', function () {
+			var called = false;
+			function callback(ret) {
+				called = true;
+			}
+
+			callerBack(callback, "Hey!");
+
+			expect(called).toBeTruthy();
+		});
+		it('should call me back with the right message', function () {
+			var calledMsg = "";
+			function callback(ret) {
+				calledMsg = ret;
+			}
+
+      callerBack(callback, "Hey!");
+			expect(calledMsg).toBe('Hey! back');
+		});
+	});
+});
